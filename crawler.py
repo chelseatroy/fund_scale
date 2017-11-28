86466b87a22731cdac8f6a021b99b7706d4ed601
chelseatroy <chelsea.dommert@gmail.com>
2017-11-27 19:59:44 -0600

    Store the URL and scraped HTML together

--- crawler.py
+++ crawler.py
@@ -5,14 +5,17 @@ from article_urls import ARTICLES
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = ARTICLES
+    url_index = 0
 
     def parse(self, response):
-        for quote in response.css('body'):
+        for html_text in response.css('body'):
             yield {
-                'text': quote.css('div').extract_first()
+                'url': self.start_urls[self.url_index],
+                'text': html_text.css('div').extract_first()
             }
 
         next_page = response.css('li.next a::attr("href")').extract_first()
         if next_page is not None:
+            self.url_index = self.url_index + 1
             yield response.follow(next_page, self.parse)
         print(self.start_urls)
